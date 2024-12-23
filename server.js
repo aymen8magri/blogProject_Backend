c1a9287f956c535d3251e76983b9d885135dce3a
aymenMagri <aymen00magri@gmail.com>
2024-12-23 18:28:18 +0100

    modification final

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
-const express = require("express");
-require('./config/connect');
+const express = require("express");///import express module
+require('./config/connect'); // connect to database
+const cors = require('cors'); //import cors module for cross-origin requests
 
-const app = express();
-app.use(express.json());
+const app = express(); //create an instance of express module  
+app.use(express.json());    // to parse JSON bodies in the request
 
-
-const articleApi = require("./routes/article");
-app.use('/article', articleApi);
-app.use('/getimage', express.static('./uploads'));
-
-const authorApi = require("./routes/author");
-app.use('/author', authorApi);
+app.use(cors());// pour autoriser les requetes cross-origin (CORS)
 
 
+const articleApi = require("./routes/article"); //import article routes qui est responsable de la gestion des articles
+app.use('/article', articleApi); //use article routes
+app.use('/getimage', express.static('./uploads')); //serveur pour les images uploadées
+
+const authorApi = require("./routes/author"); //import author routes qui est responsable de la gestion des auteurs
+app.use('/author', authorApi); //use author routes
 
 
 
+
+//start server on port 3000
 app.listen(3000, () => {
     console.log("server work");
 })
